31553f7c43027092ff555b0306261374fb2f161f
Gleb Rysev <glebrysev@gmail.com>
2021-06-21 11:19:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,39 +2,58 @@ from database import DBConnection
 from api import APIConnection
 
 import json
+from timeit import default_timer as timer
 import time
 
-def main():
-    database = DBConnection()
-    api = APIConnection()
-    while True:
-        players = database.get_players() 
+class Scraper():
+    def __init__(self):
+        self.start = timer()
+        self.requests = 0
+
+    def start_scraping(self):
+        database = DBConnection()
+        api = APIConnection()
         
-        for each in players:
-            puuid = each['puuid']
-            print(f"Getting matches for player {puuid}")
-            matches = api.get_player_matches(puuid)
-            print(matches)
-            for matchid in matches:
-                print(matchid)
-                if not database.match_exists(matchid):
-                    print(f"Match {matchid} does not exist. Adding...")
-                    match_data, status_code = api.get_match_data(matchid)
+        while True:
+            players = database.get_players() 
+            
+            for each in players:
+                puuid = each['puuid']
+                print(f"Getting matches for player {puuid}")
+                matches = api.get_player_matches(puuid)
+                check_rate_limit()
 
-                    print(match_data)
-                    if status_code == 200:
-                        player1, player2 = match_data["metadata"]["participants"]
-                        
-                        player1_data = api.get_player_data(player1)
-                        player2_data = api.get_player_data(player2)
+                for matchid in matches:
+                    if not database.match_exists(matchid):
+                        print(f"Match {matchid} does not exist. Adding...")
+                        match_data, status_code = api.get_match_data(matchid)
+                        check_rate_limit()
 
-                        database.insert_players(player1_data)
-                        database.insert_players(player2_data)
-                        
-                        database.insert_matches(match_data)
+                        if status_code == 200:
+                            player1, player2 = match_data["metadata"]["participants"]
+                            
+                            player1_data = api.get_player_data(player1)
+                            player2_data = api.get_player_data(player2)
 
-        time.sleep(600)
+                            database.insert_players(player1_data)
+                            database.insert_players(player2_data)
+                            
+                            database.insert_matches(match_data)
+
+    def check_rate_limit(self):
+        self.requests += 1
+
+        difference = timer() - self.start
+        if difference > 120:
+            self.requests = 0
+            self.start = timer()
+        else:
+            if self.requests >= 200:
+                time.sleep(difference)
+                print(f"Rate limit reached, waiting for {difference:.0f} secs")
+
 
 if __name__ == "__main__":
-    main()
+    scraper = Scraper()
+    scraper.start_scraping()
     
