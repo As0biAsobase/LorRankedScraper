8ccb5e8d499ce3ef5fcfdf39149d37699663c872
Gleb Rysev <glebrysev@gmail.com>
2021-06-22 16:35:04 +0100

    Now only search for players we know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from database import DBConnection
-from api import APIConnection
+from self.database import DBConnection
+from self.api import APIConnection
 
 import json
 from timeit import default_timer as timer
@@ -13,50 +13,30 @@ class Scraper():
         self.match_list_counter = 0
         self.match_data_counter = 0
         self.hourly_clock = timer()
+        self.database = DBConnection()
+        self.api = APIConnection()
 
     def start_scraping(self):
-        database = DBConnection()
-        api = APIConnection()
-
-        offset = 0
-
         while True:
-            players = database.get_players() 
-            random.shuffle(players)
+            players = self.database.get_players() 
             
             for each in players:
                 puuid = each['puuid']
-                print(f"Getting matches for player {puuid}")
-                matches = api.get_player_matches(puuid)
+                print(f"Getting matches for player {each["gameName"]} with id: {puuid}")
+                matches = self.api.get_player_matches(puuid)
                 self.match_list_counter += 1
                 self.check_rate_limit()
 
 
                 for matchid in matches:
-                    if not database.match_exists(matchid):
+                    if not self.database.match_exists(matchid):
                         print(f"Match {matchid} does not exist. Adding...")
-                        match_data, status_code = api.get_match_data(matchid)
+                        match_data, status_code = self.api.get_match_data(matchid)
                         self.match_data_counter += 1
                         self.check_rate_limit()
 
-                        if status_code == 200:
-                            player1, player2 = match_data["metadata"]["participants"]
-                            
-                            if not database.player_exists(player1):
-                                print(f"Never met player {player1} before, adding him...")
-                                player1_data, code = api.get_player_data(player1)
-                                self.check_rate_limit()
-                                if code == 200:
-                                    database.insert_players(player1_data)
-                            if not database.player_exists(player2):
-                                print(f"Never met player {player2} before, adding him...")
-                                player2_data, code = api.get_player_data(player2)
-                                self.check_rate_limit()
-                                if code == 200:
-                                    database.insert_players(player2_data)
-                            
-                            database.insert_matches(match_data) 
-            time.sleep(600)          
+                        if status_code == 200:                            
+                            self.database.insert_matches(match_data)        
 
 
     def check_rate_limit(self):
@@ -64,7 +44,7 @@ class Scraper():
 
         difference = timer() - self.start
         if self.match_list_counter == 200 or self.match_data_counter == 100:
-            difference = 3660 - (timer() - self.hourly_clock)
+            difference = 3600 - (timer() - self.hourly_clock)
             print(f"Rate limit reached, waiting for {difference:.0f} secs")
             time.sleep(difference)
             self.start = timer()
